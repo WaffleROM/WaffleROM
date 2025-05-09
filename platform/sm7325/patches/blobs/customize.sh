bea7a609b5f43839ebf1b461439db4e80e60bbad
Yagzie <yagzie11@gmail.com>
2025-05-09 22:01:56 +0300

    platform: sm7325: fix remaster

---
+++ platform/sm7325/patches/blobs/customize.sh
@@ -0,0 +1,3 @@
+echo "Add PhotoRemasterService"
+DELETE_FROM_WORK_DIR "system" "system/priv-app/PhotoRemasterService/oat"
+ADD_TO_WORK_DIR "a73xqxx" "system" "system/priv-app/PhotoRemasterService/PhotoRemasterService.apk"
