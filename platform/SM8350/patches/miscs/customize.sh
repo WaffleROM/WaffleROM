b3f89a21b2936250dfaa87b078903769e0059e67
Yagzie <yagzie11@gmail.com>
2025-05-11 18:51:36 +0300

    platform: SM8350: fix bluetooth

--- platform/SM8350/patches/miscs/customize.sh
+++ platform/SM8350/patches/miscs/customize.sh
@@ -21,3 +21,6 @@ ADD_TO_WORK_DIR "a73xqxx" "vendor" "bin/hw/vendor.samsung.hardware.light-service
 ADD_TO_WORK_DIR "a73xqxx" "vendor" "lib64/android.hardware.light-V1-ndk_platform.so"
 ADD_TO_WORK_DIR "a73xqxx" "vendor" "lib64/vendor.samsung.hardware.light-V1-ndk_platform.so"
 # will be useless after one ui 7 release.
+
+echo "Replace btservices APEX"
+ADD_TO_WORK_DIR "a73xqxx" "system" "system/apex/com.android.btservices.apex"
