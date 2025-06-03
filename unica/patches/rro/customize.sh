5e7d5cfeded14b7705f85bf21dd39adcbfe28df2
Ksawlii <ksawery.blaszczak@proton.me>
2025-06-03 20:54:11 +0200

    unica: patches: rro: add support for essi
    Signed-off-by: waffleowo <featherusuwu@gmail.com>

--- unica/patches/rro/customize.sh
+++ unica/patches/rro/customize.sh
@@ -1,9 +1,18 @@
+if [ "$TARGET_SINGLE_SYSTEM_IMAGE" = "qssi" ]; then
+    CODENAME="dm1qxxx"
+elif [ "$TARGET_SINGLE_SYSTEM_IMAGE" = "essi" ]; then
+    CODENAME="r11sxxx"
+else
+    echo "\"$TARGET_SINGLE_SYSTEM_IMAGE\" is not a valid system image."
+    exit 1
+fi
+
 if [ -d "$SRC_DIR/target/$TARGET_CODENAME/overlay" ]; then
-    DECODE_APK "/product/overlay/framework-res__dm1qxxx__auto_generated_rro_product.apk"
+    DECODE_APK "/product/overlay/framework-res__$CODENAME__auto_generated_rro_product.apk"
 
     echo "Applying stock overlay configs"
-    rm -rf "$APKTOOL_DIR/product/overlay/framework-res__dm1qxxx__auto_generated_rro_product.apk/res"
+    rm -rf "$APKTOOL_DIR/product/overlay/framework-res__$CODENAME__auto_generated_rro_product.apk/res"
     cp -a --preserve=all \
         "$SRC_DIR/target/$TARGET_CODENAME/overlay" \
-        "$APKTOOL_DIR/product/overlay/framework-res__dm1qxxx__auto_generated_rro_product.apk/res"
+        "$APKTOOL_DIR/product/overlay/framework-res__$CODENAME__auto_generated_rro_product.apk/res"
 fi
